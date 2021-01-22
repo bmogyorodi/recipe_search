9347eec632a067273e3e11aa8db4e50b106299d8
Dimitris Christodoulou <demetrischr1998@gmail.com>
2021-01-22 14:40:09 +0000

    Add scraper for thehappyfoodie.co.uk

---
+++ project/data/scrapers/thehappyfoodie.py
@@ -0,0 +1,13 @@
+from ._generic import SitemapScraper
+
+
+class TheHappyFoodieScraper(SitemapScraper):
+    """
+    A scraper for thehappyfoodie.co.uk
+    """
+
+    NAME = "thehappyfoodie"
+    RECIPE_URL_FORMAT = "https://thehappyfoodie.co.uk/recipes/{id}/"
+    RECIPE_URL_RE = r"https://thehappyfoodie.co.uk/recipes/(?P<id>[^/]+)$"
+
+    SITEMAP_URL = "https://thehappyfoodie.co.uk/sitemap.xml"
