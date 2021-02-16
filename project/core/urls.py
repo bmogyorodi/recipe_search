a8ace1231b7f8f3fc31bdfc2745c7f70db355997
Fasand <peter@petermanas.com>
2021-02-16 10:28:36 +0100

    fix error in core.urls by commenting out string

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,12 +1,12 @@
-from django.urls import path,re_path
+from django.urls import path, re_path
 from . import views
 
 app_name = "core"
 urlpatterns = [
-path('', views.home, name="home"),
-"""re_path(
-        r'^ingredient-autocomplete/$',
-        views.IngredientAutoComplete.as_view(),
-        name='ingredient-autocomplete',
-    )"""
+    path('', views.home, name="home"),
+    # re_path(
+    #         r'^ingredient-autocomplete/$',
+    #         views.IngredientAutoComplete.as_view(),
+    #         name='ingredient-autocomplete',
+    #     )
 ]
