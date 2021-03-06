cfd37fd4b3cd67845ff0bc3dc74ab1e42a80082f
Dimitris Christodoulou <demetrischr1998@gmail.com>
2021-03-06 18:39:44 +0000

    Correct spelling before search

--- project/data/search.py
+++ project/data/search.py
@@ -1,4 +1,5 @@
 from .indexer import Indexer
+from .spelling import SpellChecker
 from .models import RecipeToken, Recipe
 from django.db.models import Count
 from collections import defaultdict
@@ -9,9 +10,14 @@ class RankedSearch:
     def __init__(self):
         self.indexer = Indexer()
         self.doc_count = Recipe.objects.count()
+        self.spell_checker = SpellChecker()
 
     def search(self, query):
         tokens = self.indexer._preprocess_text(query)
+
+        # Correct the spelling of the tokens
+        tokens = self.spell_checker.correct_spelling(tokens)
+
         scores = defaultdict(float)
         for token in tokens:
             # Find the document frequency (df) from the inverted index
