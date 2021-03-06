42b9368cd03899acfac28cc4b316ef2d9089bea4
Dimitris Christodoulou <demetrischr1998@gmail.com>
2021-03-06 17:50:34 +0000

    Make the spell checker faster - dictionary of token counts computed at import

--- project/data/spelling.py
+++ project/data/spelling.py
@@ -1,57 +1,75 @@
-from django.db.models import Count, Max
+from django.db.models import Count
 from .models import Token
 import string
 
 
-def is_known(token):
-    return Token.objects.filter(title=token).exists()
+class SpellChecker:
+    token_counts = dict(Token.objects.annotate(token_count=Count("recipes"))
+                                     .values_list("title", "token_count"))
 
+    def _is_known(self, token):
+        """
+        Checks whether the given token exists within the database
+        """
+        return token in self.token_counts
 
-def get_token_objects_for_known_tokens(tokens):
-    return Token.objects.filter(title__in=tokens)
-
-
-def find_most_common_correct_token_spelling(token):
-    candidates = get_candidates(token)
-    if candidates:
-        candidate_counts = candidates.annotate(token_count=Count("recipes"))
-        max_count = candidate_counts.aggregate(maxcount=Max("token_count"))["maxcount"]
-        most_common_token = candidate_counts.filter(token_count=max_count)
-        return most_common_token[0].title
-    else:
-        return None
-
-
-def get_candidates(token):
-    tokens_within_2_edits = tokens_1_edit_away(token) | tokens_2_edits_away(token)
-    return get_token_objects_for_known_tokens(tokens_within_2_edits)
-
-
-def correct_spelling(tokens):
-    res = []
-    for i in range(len(tokens)):
-        token = tokens[i]
-        if is_known(token):
-            res.append(token)
+    def _find_most_common_correct_token_spelling(self, token):
+        """
+        Finds the most common token that is at most 2 edit distances away from the givent token
+        """
+        candidates = self._get_candidates(token)
+        if candidates:
+            most_common_token = None
+            max_count = 0
+            for candidate in candidates:
+                candidate_count = self.token_counts[candidate]
+                if candidate_count > max_count:
+                    most_common_token = candidate
+                    max_count = candidate_count
+            return most_common_token
         else:
-            correct_token = find_most_common_correct_token_spelling(token)
-            if correct_token is not None:
-                res.append(correct_token)
-    return res
+            return None
 
+    def _get_candidates(self, token):
+        """
+        Finds all tokens within at most two edit distances that exist in the database
+        """
+        tokens_within_2_edits = self._tokens_1_edit_away(token) | self._tokens_2_edits_away(token)
+        return tokens_within_2_edits & self.token_counts.keys()
 
-def tokens_2_edits_away(token):
-    res = set()
-    for e1 in tokens_1_edit_away(token):
-        res |= tokens_1_edit_away(e1)
-    return res
+    def _tokens_2_edits_away(self, token):
+        """
+        Finds all tokens that are exactly two edit distances away
+        """
+        res = set()
+        for e1 in self._tokens_1_edit_away(token):
+            res |= self._tokens_1_edit_away(e1)
+        return res
 
+    def _tokens_1_edit_away(self, token):
+        """
+        Finds all tokens that are exactly one edit distance away
+        """
+        chars = string.ascii_lowercase + "'"
+        splits = [(token[:i], token[i:]) for i in range(len(token) + 1)]
+        deletes = [L + R[1:] for L, R in splits if R]
+        transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
+        replaces = [L + c + R[1:] for L, R in splits if R for c in chars]
+        inserts = [L + c + R for L, R in splits for c in chars]
+        return set(deletes + transposes + replaces + inserts)
 
-def tokens_1_edit_away(token):
-    chars = string.ascii_lowercase + "'"
-    splits = [(token[:i], token[i:]) for i in range(len(token) + 1)]
-    deletes = [L + R[1:] for L, R in splits if R]
-    transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R) > 1]
-    replaces = [L + c + R[1:] for L, R in splits if R for c in chars]
-    inserts = [L + c + R for L, R in splits for c in chars]
-    return set(deletes + transposes + replaces + inserts)
+    def correct_spelling(self, tokens):
+        """
+        Corrects the spelling of each of the tokens. If the spelling is correct the token is left
+        unchanged. If a correct spelling cannot be found, the token is discarded
+        """
+        res = []
+        for i in range(len(tokens)):
+            token = tokens[i]
+            if self._is_known(token):
+                res.append(token)
+            else:
+                correct_token = self._find_most_common_correct_token_spelling(token)
+                if correct_token is not None:
+                    res.append(correct_token)
+        return res
