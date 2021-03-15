068347e54cba68295923a93f634d99152748b3f5
Fasand <peter@petermanas.com>
2021-03-15 09:10:44 +0100

    add version.py and load static files using the ?v= tag to refresh local cache on changes

--- project/core/templatetags/utils.py
+++ project/core/templatetags/utils.py
@@ -7,3 +7,15 @@ register = template.Library()
 @register.filter
 def round_number(num):
     return round(num)
+
+
+@register.simple_tag
+def version():
+    from django.conf import settings
+    return settings.VERSION
+
+
+@register.simple_tag
+def commit_id():
+    from django.conf import settings
+    return settings.COMMIT_ID
---
+++ project/project/version.py
@@ -0,0 +1,28 @@
+import subprocess
+from subprocess import PIPE
+
+try:
+    # git rev-parse --abbrev-ref HEAD
+    BRANCH = subprocess.run(
+        ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+        stdout=PIPE, stderr=PIPE).stdout.decode('utf-8').strip()
+    # git rev-parse --short HEAD
+    COMMIT_ID = subprocess.run(
+        ['git', 'rev-parse', '--short', 'HEAD'],
+        stdout=PIPE, stderr=PIPE).stdout.decode('utf-8').strip()
+    # git rev-list --count HEAD
+    NUM_COMMITS = int(subprocess.run(
+        ['git', 'rev-list', '--count', 'HEAD'],
+        stdout=PIPE, stderr=PIPE).stdout)
+
+    VERSION = f"{BRANCH}@{COMMIT_ID}@{NUM_COMMITS}"
+
+    # git log -1 --date=format:"%Y/%m/%d %T" --format="%ad"
+    VERSION_DATETIME = subprocess.run(
+        ['git', 'log', '-1', '--date=format:"%d. %m. %Y %T"', '--format="%ad"'],
+        stdout=PIPE, stderr=PIPE).stdout.decode('utf-8').strip(
+    ).replace('"', '')
+
+except:
+    VERSION = "Version cannot be loaded"
+    VERSION_DATETIME = "Commit datetime cannot be loaded"
